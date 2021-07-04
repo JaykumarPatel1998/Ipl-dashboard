c7cc795b5fffc981f50744eed3a3ffd91d2092bf
Jaykumar <jaykumarpatel2710@gmail.com>
2021-07-04 13:30:30 +0530

    added backend match api

--- src/main/java/io/jaykumar/ipldashboard/controller/TeamController.java
+++ src/main/java/io/jaykumar/ipldashboard/controller/TeamController.java
@@ -2,11 +2,17 @@ package io.jaykumar.ipldashboard.controller;
 
 
 
+import java.time.LocalDate;
+
+import java.util.List;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jaykumar.ipldashboard.model.Match;
 import io.jaykumar.ipldashboard.model.Team;
 import io.jaykumar.ipldashboard.repository.MatchRepository;
 import io.jaykumar.ipldashboard.repository.TeamRepository;
@@ -37,6 +43,20 @@ public class TeamController {
       team.setMatches(this.matchRepository.findLatestMatchesByTeam(teamName, 4));
 
       return team;
+    }
+
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatchesForTeam(@PathVariable String teamName, @RequestParam int year){
+      LocalDate startDate = LocalDate.of(year, 1, 1);
+      LocalDate endDate = LocalDate.of(year +1, 1,1);
+     return this.matchRepository.getMatchesByTeamBetweenDates(
+        teamName, 
+        startDate,
+        endDate
+       
+        );
+      
     }
     
 }
